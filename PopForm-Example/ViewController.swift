387375ee08ded9e8c55e57d1627e52bf476a3e93
Dan Koza MacBook Pro <danieljkoza@gmail.com>
2021-07-28 21:29:00 -0500

    Initial Commit

---
+++ PopForm-Example/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Form
+//
+//  Created by Dan Koza on 7/25/21.
+//
+
+import UIKit
+import ValidatableForm
+
+#warning("see if you can fix when changing a phone number the cursor jumps to the end when textField.text is changed")
+
+class ViewController: UIViewController, ValidatableFormDelegate {
+
+    private var form: ValidatableForm?
+
+    let textField1: UIFormTextField = {
+        let textField = UIFormTextField()
+        textField.setValidation {
+            guard !(textField.text?.isEmpty ?? true) else { return .unknown }
+            return textField.text?.lowercased() == textField.placeholder ? .valid : .invalid
+        }
+        textField.setValidationStateDidChangeHandler {
+            switch textField.validationState {
+                case .unknown:
+                    textField.backgroundColor = nil
+                case .invalid:
+                    textField.backgroundColor = .red
+                case .valid:
+                    textField.backgroundColor = .green
+            }
+        }
+        textField.placeholder = "first"
+        return textField
+    }()
+
+    let textField2: UIFormTextField = {
+        let textField = UIFormTextField()
+        textField.setValidation {
+            guard !(textField.text?.isEmpty ?? true) else { return .unknown }
+            return textField.text?.lowercased() == textField.placeholder ? .valid : .invalid
+        }
+        textField.setValidationStateDidChangeHandler {
+            switch textField.validationState {
+                case .unknown:
+                    textField.backgroundColor = nil
+                case .invalid:
+                    textField.backgroundColor = .red
+                case .valid:
+                    textField.backgroundColor = .green
+            }
+        }
+        textField.placeholder = "second"
+        return textField
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        form = ValidatableForm(fields: textField1, textField2, delegate: self)
+
+        let stackView = UIStackView(arrangedSubviews: [textField1, textField2])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+
+    func formValidationChanged(formIsValid: Bool) {
+        print("formIsValid: \(formIsValid)")
+    }
+}
